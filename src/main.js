08159fa8963287088b1f3271ed364a5221eb0050
killian-dv <killian.david@etudiant.univ-reims.fr>
2025-01-20 23:20:45 +0100

    ✨ : add spot light shadow

--- src/main.js
+++ src/main.js
@@ -18,12 +18,12 @@ const scene = new THREE.Scene();
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
 gui.add(ambientLight, "intensity").min(0).max(3).step(0.001);
 scene.add(ambientLight);
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(2, 2, -1);
 gui.add(directionalLight, "intensity").min(0).max(3).step(0.001);
 gui.add(directionalLight.position, "x").min(-5).max(5).step(0.001);
@@ -39,13 +39,28 @@ directionalLight.shadow.camera.left = -2;
 directionalLight.shadow.camera.right = 2;
 directionalLight.shadow.camera.top = 2;
 directionalLight.shadow.camera.bottom = -2;
-directionalLight.shadow.radius = 10; // blur
+// directionalLight.shadow.radius = 10; // blur
 
 const directionalLightCameraHelper = new THREE.CameraHelper(
   directionalLight.shadow.camera
 );
 directionalLightCameraHelper.visible = false;
 scene.add(directionalLightCameraHelper);
+
+// spot light
+const spotLight = new THREE.SpotLight(0xffffff, 3.6, 10, Math.PI * 0.3);
+spotLight.castShadow = true;
+spotLight.position.set(0, 2, 2);
+spotLight.shadow.mapSize.set(1024, 1024);
+spotLight.shadow.camera.near = 1;
+spotLight.shadow.camera.far = 6;
+spotLight.shadow.camera.fov = 30;
+scene.add(spotLight);
+scene.add(spotLight.target);
+
+const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
+spotLightCameraHelper.visible = false;
+scene.add(spotLightCameraHelper);
 
 /**
  * Materials
