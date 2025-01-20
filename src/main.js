7b85cddcf5a72a17e09a1a0af3361adeafad903a
killian-dv <killian.david@etudiant.univ-reims.fr>
2025-01-20 23:12:40 +0100

    ✨: add shadow and optimize it

--- src/main.js
+++ src/main.js
@@ -31,6 +31,22 @@ gui.add(directionalLight.position, "y").min(-5).max(5).step(0.001);
 gui.add(directionalLight.position, "z").min(-5).max(5).step(0.001);
 scene.add(directionalLight);
 
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.near = 1;
+directionalLight.shadow.camera.far = 6;
+directionalLight.shadow.camera.left = -2;
+directionalLight.shadow.camera.right = 2;
+directionalLight.shadow.camera.top = 2;
+directionalLight.shadow.camera.bottom = -2;
+directionalLight.shadow.radius = 10; // blur
+
+const directionalLightCameraHelper = new THREE.CameraHelper(
+  directionalLight.shadow.camera
+);
+directionalLightCameraHelper.visible = false;
+scene.add(directionalLightCameraHelper);
+
 /**
  * Materials
  */
@@ -43,10 +59,12 @@ gui.add(material, "roughness").min(0).max(1).step(0.001);
  * Objects
  */
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
+sphere.castShadow = true;
 
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
 plane.rotation.x = -Math.PI * 0.5;
 plane.position.y = -0.5;
+plane.receiveShadow = true;
 
 scene.add(sphere, plane);
 
@@ -101,6 +119,8 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 // activate shadow
 renderer.shadowMap.enabled = true;
+// activate soft shadow
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 /**
  * Animate
